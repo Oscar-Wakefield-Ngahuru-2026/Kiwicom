e54aa378a79280cd16e1950b11da34abcbbb5231
Oscar <oscarwakefield16@gmail.com>
2026-06-28 16:18:09 +1200

    display the projectnmae on the project card, delete id increment migrations file

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -40,7 +40,7 @@ export default function Home() {
           <li key={project.id}>
             <ProjectCard
               id={project.id}
-              name={project.}
+              name={project.name}
               description={project.description}
               ownerName={project.ownerName}
               githubUrl={project.githubUrl}
