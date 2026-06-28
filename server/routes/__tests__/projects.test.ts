33ef9f6bc77eb7fb7eb1833637d46f166b1ff444
Oscar <oscarwakefield16@gmail.com>
2026-06-28 16:45:03 +1200

    fix tests to accept camelCase

--- server/routes/__tests__/projects.test.ts
+++ server/routes/__tests__/projects.test.ts
@@ -33,8 +33,8 @@ describe('GET /api/v1/projects', () => {
     expect(res.status).toBe(200)
     expect(res.body).toHaveLength(1)
     expect(res.body[0]).toMatchObject({
-      full_name: 'Oscar-Wakefield-Ngahuru-2026/Kiwicom',
-      html_url: 'https://github.com/Oscar-Wakefield-Ngahuru-2026/Kiwicom',
+      fullName: 'Oscar-Wakefield-Ngahuru-2026/Kiwicom',
+      htmlUrl: 'https://github.com/Oscar-Wakefield-Ngahuru-2026/Kiwicom',
     })
   })
 
