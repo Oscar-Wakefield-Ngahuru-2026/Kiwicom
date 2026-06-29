8dfd4bc1c68c286c178145dcd447dff9247eeba7
Serina Mcfall <serina.mcfall@gmail.com>
2026-06-29 14:35:16 +1200

    feat: wire /developers/:username route to DeveloperProfile page (#18)

--- client/router.tsx
+++ client/router.tsx
@@ -8,12 +8,14 @@ import Layout from './components/Layout'
 import Home from './pages/Home'
 import ProjectPage from './pages/ProjectPage'
 import CreateProject from './pages/CreateProject'
+import DeveloperProfile from './pages/DeveloperProfile'
 
 export const routes = createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
     <Route path="projects/:id" element={<ProjectPage />} />
     <Route path="projects/new" element={<CreateProject />} />
+    <Route path="developers/:username" element={<DeveloperProfile />} />
   </Route>,
 )
 
