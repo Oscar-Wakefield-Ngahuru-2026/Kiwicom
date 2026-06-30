a8bee6b9b20826c6afd4d1d2acb56c491ad06383
Serina Mcfall <serina.mcfall@gmail.com>
2026-06-30 16:32:33 +1200

    feat: add bookmark apiClient functions

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -77,26 +77,47 @@ export async function getProfileByUsername(
 export async function updateProfile(
   id: string,
   data: Partial<DeveloperProfile>,
-  ): Promise<DeveloperProfile> {
-    const res = await request.patch(`${rootURL}/profiles/${id}`).send(data)
-    const profile = res.body
-    return {
-      ...profile,
-      githubLink:
-        profile.githubLink ?? `https://github.com/${profile.githubUsername}`,
-    }
+): Promise<DeveloperProfile> {
+  const res = await request.patch(`${rootURL}/profiles/${id}`).send(data)
+  const profile = res.body
+  return {
+    ...profile,
+    githubLink:
+      profile.githubLink ?? `https://github.com/${profile.githubUsername}`,
   }
+}
 
-  export async function updateSocialLinks(
-    id: string,
-    links: { label: string; url: string}[]
-  ): Promise<void> {
-    await request.put(`${rootURL}/profiles/${id}/social-links`).send(links)
-  }
+export async function updateSocialLinks(
+  id: string,
+  links: { label: string; url: string }[],
+): Promise<void> {
+  await request.put(`${rootURL}/profiles/${id}/social-links`).send(links)
+}
 
 export async function addProject(data: ProjectData): Promise<Project> {
   const res = await request.post(`${rootURL}/projects`).send(data)
   return res.body
+}
+
+export async function getBookmarkedProjects(
+  userId: string,
+): Promise<Project[]> {
+  const res = await request.get(`${rootURL}/bookmarks/${userId}`)
+  return res.body as Project[]
+}
+
+export async function addBookmark(
+  userId: string,
+  projectId: number,
+): Promise<void> {
+  await request.post(`${rootURL}/bookmarks`).send({ userId, projectId })
+}
+
+export async function removeBookmark(
+  userId: string,
+  projectId: number,
+): Promise<void> {
+  await request.delete(`${rootURL}/bookmarks/${userId}/${projectId}`)
 }
 
 export type { Project }
